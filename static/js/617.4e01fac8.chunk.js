8c5ca64abd53baf3e93571f84043c4a6e37b9e7d
Mysolaar <110239706+Mysolaar@users.noreply.github.com>
2023-04-12 13:27:07 +0000

    Deploying to gh-pages from @ Mysolaar/goit-react-hw-05-movies@2dec7e389a3735080886326081672fbe423883a6 🚀

--- static/js/617.4e01fac8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[617],{8617:function(t,e,r){r.d(e,{Tsu:function(){return o}});var n=r(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M10 19l-7-7m0 0l7-7m-7 7h18"}}]})(t)}}}]);
-//# sourceMappingURL=617.4e01fac8.chunk.js.map
